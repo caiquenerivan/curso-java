2a2f6a763e5ba95c2df02a2f5bb5655518ea226d
Caique Nerivan <caiquenerivansantos@gmail.com>
2022-02-04 21:03:44 -0300

    primeiro commit

---
+++ CursoJavaPS/src/PortoSeguro/OlaMundo.java
@@ -0,0 +1,27 @@
+package PortoSeguro;
+
+import java.util.Scanner;
+
+public class OlaMundo{
+	
+	public static void main(String[] args) {
+		
+		String name;
+		int idade = 27;
+		float altura;
+		
+		Scanner leia = new Scanner(System.in);
+		
+		System.out.println("Fala fiote");
+		
+		System.out.println("\nQual o nome: ");
+		
+		name = leia.nextLine();
+		System.out.println("\nQual a tua altura");
+		
+		altura = leia.nextFloat();
+		
+		System.out.println("\n hahahahhahaha o nome do cara é "+name+" puta nego tonto véi,  o cara tem "+idade+ " anos e "+altura+" de altura, menor que o pedrinho");
+	}
+	
+}
